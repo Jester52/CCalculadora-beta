81dcf7b9e632d5a2cbe45a300bd608b6c356f558
Luis Torres <74846261+Jester52@users.noreply.github.com>
2026-03-15 18:18:37 -0500

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,39 @@
+const CACHE_NAME = 'cronos-magic-v1';
+const urlsToCache = [
+  './',
+  './index.html',
+  './test_calculadora_Android.html',
+  './test_calculadora_IOS.html',
+  './configuracion.html',
+  './instrucciones.html',
+  './manifest.json'
+];
+
+// Instalación del Service Worker
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+// Estrategia: Buscar en caché, si no hay, ir a la red
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
+});
+
+// actualizado forzado
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cache) => {
+          return caches.delete(cache); // Borra toda la caché vieja al activar
+        })
+      );
+    })
+  );
+});
